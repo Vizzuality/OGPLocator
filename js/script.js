28e0e6ca8d4b4398d8ac57acf3ffbd1abde954d4
Fernando Espinosa <fer@ferdev.com>
2012-04-07 18:34:11 +0200

    Shows proper controls in maps

--- js/script.js
+++ js/script.js
@@ -7,13 +7,27 @@ var mini_map = null;
 var map_options = {
   zoom: 3,
   center: new google.maps.LatLng(34.397, 0),
-  mapTypeId: google.maps.MapTypeId.TERRAIN
+  mapTypeId: google.maps.MapTypeId.TERRAIN,
+  panControl: false,
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  overviewMapControl: false,
+  zoomControlOptions: {
+    position: google.maps.ControlPosition.RIGHT_TOP
+  }
 };
 
 var mini_map_options = {
   zoom: 8,
   center: new google.maps.LatLng(34.397, 0),
-  mapTypeId: google.maps.MapTypeId.TERRAIN
+  mapTypeId: google.maps.MapTypeId.TERRAIN,
+  panControl: false,
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  overviewMapControl: false,
+  zoomControl: false
 };
 
 $(function(){
