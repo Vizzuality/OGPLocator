9c680e359525e43006835a30400cf1c19a6b7fe9
Fernando Espinosa <fer@ferdev.com>
2012-04-12 10:51:37 +0200

    Shows all topcis and challenges in filters

--- js/backbone/collections/topics.js
+++ js/backbone/collections/topics.js
@@ -2,8 +2,7 @@ var TopicsCollection = CartoDB.CartoDBCollection.extend({
   sql: function(){
     return ' \
       SELECT DISTINCT t.cartodb_id, t.name \
-      FROM ogp_topics t \
-      INNER JOIN case_studies cs ON cs.topic_id = t.cartodb_id';
+      FROM ogp_topics t';
   },
 
   getByCartoDBId: function(id){
