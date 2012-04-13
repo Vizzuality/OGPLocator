2a785d8fb43248bb2eda8708756d5b63497d1f85
Fernando Espinosa <fer@ferdev.com>
2012-04-13 15:04:51 +0200

    Fixes bad linked emails in detail page

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ var Topics            = new TopicsCollection();
 var Categories        = new CategoriesCollection();
 var Case              = null;
 
-var uri_regexp  = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/gi;
-var mail_regexp = /[0-9a-zA-Z]+@[0-9a-zA-Z]+[\.]{1}[0-9a-zA-Z]+[\.]?[0-9a-zA-Z]+/gi;
+var uri_regexp  = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\.\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/gi;
+var mail_regexp = /[0-9a-zA-Z|.]+@[0-9a-zA-Z]+[\.]{1}[0-9a-zA-Z]+[\.]?[0-9a-zA-Z]+/gi;
 
 var disqus_shortname = 'ogpdemo';
 var disqus_url = null;
