790da529b91870d5a9b48641dcea63ebc73d6ffa
dcano <dcano@vizzuality.com>
2012-04-16 19:20:27 +0200

    Disquus account changed

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var Case              = null;
 var uri_regexp  = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\.\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/gi;
 var mail_regexp = /[0-9a-zA-Z|.]+@[0-9a-zA-Z]+[\.]{1}[0-9a-zA-Z]+[\.]?[0-9a-zA-Z]+/gi;
 
-var disqus_shortname = 'ogpdemo';
+var disqus_shortname = 'ogp-experience-locator';
 var disqus_url = null;
 var disqus_developer = 1;
 //var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
